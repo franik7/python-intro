ede524f43e1c047b8379a2911752f5b988dbbd9e
Frants <118231497+franik7@users.noreply.github.com>
2024-03-26 21:25:44 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,40 @@
+print ('Hello')
+
+fruit = 'apple'
+
+fruit = 'orange'
+
+print (fruit[0])
+
+print(fruit)
+
+fruit_len = len (fruit)
+print(fruit_len)
+
+fruit2 = 'APPle'
+print(fruit2.lower())
+
+print ('I ' + 'love' + ' python.')
+
+print('-' * 10)
+
+
+print('{} {} {}'.format('I', 'love', 'Python.'))
+
+print('{0:8} | {1:<8}'.format('Fruit', 'Quantity'))
+print('{0:8} | {1:<8.2f}'.format('Apple', 2.55443))
+
+fruit = input('Enter a name of a fruit: ')
+print('{} is a lovely fruit.'.format(fruit))
+
+animal = 'cat'
+vegetable = 'carrot'
+mineral = 'gold'
+
+print('Here is an anima, a vegetable, and a mineral')
+print(animal)
+print(vegetable)
+print(mineral)
+
+
+
