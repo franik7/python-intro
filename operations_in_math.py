fdc675cdb6a0f037cacc0fcb50a70ddd61d7886d
Frants <118231497+franik7@users.noreply.github.com>
2024-03-26 21:25:54 -0400

    Add files via upload

---
+++ operations_in_math.py
@@ -0,0 +1,25 @@
+integer = 42
+float = 4.2
+
+print(2**4)
+
+sum = 1 + 2
+print ('Sum:{}'.format(sum))
+
+
+three = 3
+four = '4'
+
+
+print(three + int(four))
+
+# this is a comment
+
+"""Hello,
+multiple line comment"""
+
+a_boolean = True
+print(a_boolean)
+print(1 == 2)
+
+31 < 40
